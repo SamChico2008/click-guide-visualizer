eb4712b39416348f9499da33dfa27502a4168896
SamChico <samchico@example.com>
2026-08-01 19:04:16 -0400

    feat: implement cross-thread macro file selection using Windows common dialogs

--- src/ui/MacroPicker.cpp
+++ src/ui/MacroPicker.cpp
@@ -1,31 +1,42 @@
 #include "MacroPicker.hpp"
+#include <Geode/Geode.hpp>
 #include <windows.h>
 #include <commdlg.h>
-#include <filesystem>
+#include <thread>
+#include <atomic>
 
 namespace cgv {
 
+static std::atomic<bool> s_picking{false};
+
 void pickMacroFile(PickCallback callback) {
-    char szFile[MAX_PATH] = {0};
+    if (s_picking.exchange(true)) return;
 
-    OPENFILENAMEA ofn;
-    ZeroMemory(&ofn, sizeof(ofn));
-    ofn.lStructSize = sizeof(ofn);
-    ofn.hwndOwner = NULL;
-    ofn.lpstrFile = szFile;
-    ofn.nMaxFile = sizeof(szFile);
-    ofn.lpstrFilter = "Macro Files (*.gdr;*.gdr2;*.json;*.mhr)\0*.gdr;*.gdr2;*.json;*.mhr\0All Files (*.*)\0*.*\0";
-    ofn.nFilterIndex = 1;
-    ofn.lpstrFileTitle = NULL;
-    ofn.nMaxFileTitle = 0;
-    ofn.lpstrInitialDir = NULL;
-    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+    std::thread([callback = std::move(callback)]() {
+        char szFile[MAX_PATH] = {0};
 
-    if (GetOpenFileNameA(&ofn) == TRUE) {
-        callback(std::filesystem::path(szFile));
-    } else {
-        callback(std::nullopt);
-    }
+        OPENFILENAMEA ofn;
+        ZeroMemory(&ofn, sizeof(ofn));
+        ofn.lStructSize = sizeof(ofn);
+        ofn.hwndOwner = GetActiveWindow();
+        ofn.lpstrFile = szFile;
+        ofn.nMaxFile = sizeof(szFile);
+        ofn.lpstrFilter = "Macro Files (*.gdr;*.gdr2;*.json;*.mhr)\0*.gdr;*.gdr2;*.json;*.mhr\0All Files (*.*)\0*.*\0";
+        ofn.nFilterIndex = 1;
+        ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+
+        BOOL res = GetOpenFileNameA(&ofn);
+        std::string chosenPath = (res == TRUE) ? std::string(szFile) : "";
+
+        geode::Loader::get()->queueInMainThread([callback, res, chosenPath]() {
+            s_picking.store(false);
+            if (res == TRUE && !chosenPath.empty()) {
+                callback(std::filesystem::path(chosenPath));
+            } else {
+                callback(std::nullopt);
+            }
+        });
+    }).detach();
 }
 
 } // namespace cgv
